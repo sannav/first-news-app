287a24e465952de4d075376b696996cd73be884f
Sanna Volny <shsavy07@suni.se>
2016-03-11 19:49:49 +0100

    ladda upp csv

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-# -*- coding: utf-8 -*-
-
-# importer
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-#  variabeln app skapa funktionen en route oot-mapp och staller sig dar
-#skapa en funktion som skapar en indexfil for hemsidan där appen ska ligga, i root-mappen
 @app.route("/")
 def index():
     template = 'index.html'
     return render_template(template)
+
 if __name__ == '__main__':
-    # Starta Flask test server i debbuging lagege
+    # Fire up the Flask test server
     app.run(debug=True, use_reloader=True)
-	
