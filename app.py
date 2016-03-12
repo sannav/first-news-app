64b3935dbcd1d4cc504958af870917e97e1a1bf4
Sanna Volny <shsavy07@suni.se>
2016-03-12 19:21:07 +0100

    Uppdatera med kartor

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-
-
 import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-#skapa funktion för att ladda in csv, . betyder i nuvarande katalog, r=read
+#skapa funktion for att ladda in csv, . betyder i nuvarande katalog, r=read
 # skapa en variabel csv-path opppna csv-filen och skapa en lista 
 def get_csv(csv_path):
     csv_file = open(csv_path, "r")
@@ -20,7 +18,7 @@ def index():
     object_list = get_csv("./static/la-riots-deaths.csv")
     return render_template(template, object_list=object_list)
 
-# funktionen nedan skapar en variabel, row_id, o talar om att raden x i csv-filen det ska filen detail.html ska användas som mall
+# funktionen nedan skapar en variabel, row_id, o talar om att raden x i csv-filen det ska filen detail.html ska anvandas som mall
 # 
 @app.route("/<row_id>/")
 def detail(row_id):
